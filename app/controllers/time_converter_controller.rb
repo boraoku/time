06699f976ba0b29642ad159cab477a1912c4d841
Bora Okumusoglu <bora@hetge.com>
2025-09-15 08:45:14 +1000

    Ajax form (no longer full page load) and prelim animation of faces

--- app/controllers/time_converter_controller.rb
+++ app/controllers/time_converter_controller.rb
@@ -11,5 +11,28 @@ class TimeConverterController < ApplicationController
         @error = "Could not parse the input. Try format like: '10pm sydney in london and dubai'"
       end
     end
+    
+    respond_to do |format|
+      format.html
+      format.json do
+        if @results.present?
+          render json: { 
+            results: @results.map do |result|
+              {
+                city: result[:city],
+                time: result[:time].strftime("%l:%M %p").strip,
+                is_pm: result[:is_pm],
+                offset: result[:offset],
+                hour: result[:time].hour,
+                minute: result[:time].min
+              }
+            end,
+            error: nil 
+          }
+        else
+          render json: { results: nil, error: @error || "Please enter a query" }
+        end
+      end
+    end
   end
 end
